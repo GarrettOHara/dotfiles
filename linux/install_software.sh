56f2ca1be5ab0dd731865128cacb3a83c15a3748
Garrett O'Hara <garrettohara2018@gmail.com>
2024-09-02 21:17:54 -0700

    adding yarn package for neovim neo-tree

--- linux/install_software.sh
+++ linux/install_software.sh
@@ -44,14 +44,14 @@ sudo apt install -y python3-dev		# Header files and a static library for Python
 # Neovim
 sudo apt install nodejs
 sudo apt install npm
-sudo apt install nodejs
-sudo apt install npm
+sudo npm install --global yarn
 
 # Install LazyGit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+rm lazygit lazygit.tar.gz
 
 # Clean up
 sudo apt autoremove
